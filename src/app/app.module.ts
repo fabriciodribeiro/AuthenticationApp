287bdce06c9ce09198c109651fef216258935317
Fabricio Ribeiro <fabriciodribeiro@hotmail.com>
2022-09-25 23:00:22 +0100

    Add the authentication service, Guard and token request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TokenInterceptor } from './helpers/token.interceptor';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,9 +14,10 @@ import { SecretComponent } from './secret/secret.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/clients/authentication.client.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AuthenticationClient {
+    constructor(private http: HttpClient) { }
+
+    public login(username: string, password: string): Observable<string> {
+        return this.http.post(
+            environment.apiUrl + '/user/login',
+            {
+                username: username,
+                password: password,
+            },
+            { responseType: 'text' }
+        );
+    }
+
+    public register(
+        username: string,
+        email: string,
+        password: string
+    ): Observable<string> {
+        return this.http.post(
+            environment.apiUrl + '/api/users/signup',
+            {
+                name: "Name",
+                username: username,
+                email: email,
+                password: password,
+            },
+            { responseType: 'text' }
+        );
+    }
+}
